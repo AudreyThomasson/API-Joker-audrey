ee326b581a23b168ee64961e493e1e55a950f7c0
Audrey Thomasson <audrey@macbook-pro.lan>
2020-08-31 22:51:43 -0500

    joke setup working

--- scripts/JokeProvider.js
+++ scripts/JokeProvider.js
@@ -1,20 +1,21 @@
 // 1. define and export a useJoke function that simply returns the joke object.
 //         Advanced: find a way to return a copy of the joke object (tip: .slice will not work)
 // 2. define and export a getJoke function that fetches a joke from the API and sets the joke variable to the response
-let joke;
+let joke = '';
 
 export const getJoke = () => {
 	return fetch('https://official-joke-api.appspot.com/jokes/random') 	
-    .then(response => response.json())
-    .then((responseData) => {
-        let joke = responseData;
-        
-    return joke
-    })
+        .then(response => response.json())
+        .then(
+            responseData => {
+                joke = responseData
+            }
+        )
 }
 
 
 
-// export const useJoke = () => {
-//     return Object.assign({}, joke);
-// }
+export const useJoke = () => {
+    return Object.assign({}, joke)
+};
+
--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-import { getJoke } from "./JokeProvider.js";
+import { getJoke, useJoke } from "./JokeProvider.js";
 import { jokeSetup } from "./Joke.js";
 
 const jokeBtn = document.querySelector(".request-joke-btn");
@@ -6,14 +6,16 @@ const jokeBtn = document.querySelector(".request-joke-btn");
 jokeBtn.addEventListener("click", (e) => {
   console.log("I should be grabbing a joke now...");
   // TODO: fetch a joke from the Joke API and render it to the DOM
-  const joke = getJoke()
-  
-  const contentElement = document.querySelector(".joke-setup");
+  getJoke()
+  .then(() => {
+    const joke = useJoke();
+
+    const contentElement = document.querySelector(".joke-setup");
     
 
     const jokeSetupHTMLRep = jokeSetup(joke);
     contentElement.innerHTML = `
         ${jokeSetupHTMLRep}  
     `
-  // }
+  })
 });
